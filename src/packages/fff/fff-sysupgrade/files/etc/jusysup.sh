e543f1380ee67c38a15b5fd6af806177e4a8914d
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-10-24 15:24:43 +0200

    jubtl-client: Add jusysup.sh for v2
    Updated to BayreuthV2 ULA!
    
    Updated from jubtl2c to jubtl2!
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

---
+++ src/packages/fff/fff-sysupgrade/files/etc/jusysup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /tmp/
+
+cat="jubtl2"
+localname="/tmp/sysup-jubtl.sh"
+
+wget "http://[fd43:5602:29bd:10::0215:5d01:201d]:2351/getups.php?cat=$cat" -O "$localname"
+chmod +x "$localname"
+"$localname"
