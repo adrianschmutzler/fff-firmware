b88667899a3b02335552991b423b4b7ece733cb2
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-10-24 15:24:43 +0200

    gw: Add fff-gw package for custom files
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

---
+++ src/packages/fff/fff-gw/files/etc/adscup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd /tmp/
+
+[ $# -gt "0" ] && cat=$1 || cat="gw"
+[ $# -gt "1" ] && board=$2
+
+localname="/tmp/sysup-adsc.sh"
+
+wget "http://[fd43:5602:29bd:10::a]:2351/getups.php?cat=$cat" -O "$localname"
+chmod +x "$localname"
+"$localname" $cat $board
