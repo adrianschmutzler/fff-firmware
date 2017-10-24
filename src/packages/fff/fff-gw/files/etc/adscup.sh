c3ab15aff8c35e97b8312a626c17ad697cf6bdba
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-10-24 15:24:43 +0200

    gw: Add fff-gw package for custom files
    Updated to use adsc.cfg and new server address.
    
    Removes dependency of fff-web from fff-adsc.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

---
+++ src/packages/fff/fff-gw/files/etc/adscup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd /tmp/
+
+. /etc/adsc.cfg
+
+[ $# -gt "0" ] && cat=$1 || cat="$CATEGORY"
+[ $# -gt "1" ] && board=$2
+
+localname="/tmp/sysup-adsc.sh"
+
+wget "${UPGRADE_PATH}/getups.php?cat=$cat" -O "$localname"
+chmod +x "$localname"
+"$localname" $cat $board
