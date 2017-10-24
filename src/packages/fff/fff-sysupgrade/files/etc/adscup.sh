1150ed923175edd64469d6ad29d6d58365bbde39
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-10-24 15:24:43 +0200

    adsc: Changed update server IPv6 address to BayreuthV2::a
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- src/packages/fff/fff-sysupgrade/files/etc/adscup.sh
+++ src/packages/fff/fff-sysupgrade/files/etc/adscup.sh
@@ -4,6 +4,6 @@ cd /tmp/
 cat="adsc2"
 localname="/tmp/sysup-adsc.sh"
 
-wget "http://[fd43:5602:29bd:10::0215:5d01:201d]:2351/getups.php?cat=$cat" -O "$localname"
+wget "http://[fd43:5602:29bd:10::a]:2351/getups.php?cat=$cat" -O "$localname"
 chmod +x "$localname"
 "$localname"
