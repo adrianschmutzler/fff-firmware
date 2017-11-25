a865e5f9da796e70750c7516c19831cc26f85ac1
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-11-25 20:55:12 +0100

    Revert "jubtl-client: Add jusysup.sh for v2"
    This reverts commit dadf31f755763d9ca5d883a8255a3bd7f700a917.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- src/packages/fff/fff-sysupgrade/files/etc/jusysup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /tmp/
-
-cat="jubtl2c"
-localname="/tmp/sysup-jubtl.sh"
-
-wget "http://[fd43:5602:29bd:10::0215:5d01:201d]:2351/getups.php?cat=$cat" -O "$localname"
-chmod +x "$localname"
-"$localname"
