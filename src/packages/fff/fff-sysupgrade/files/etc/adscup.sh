d27d33f585487b3b28d98e8785b3c232e17eb355
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2018-03-19 16:28:02 +0100

    adsc: Support arguments in adscup.sh
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- src/packages/fff/fff-sysupgrade/files/etc/adscup.sh
+++ src/packages/fff/fff-sysupgrade/files/etc/adscup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 cd /tmp/
 
-cat="adsc2"
+[ $# -gt "0" ] && cat=$1 || cat="adsc2"
+[ $# -gt "1" ] && board=$2
+
 localname="/tmp/sysup-adsc.sh"
 
 wget "http://[fd43:5602:29bd:10::a]:2351/getups.php?cat=$cat" -O "$localname"
 chmod +x "$localname"
-"$localname" "$cat"
+"$localname" $cat $board
