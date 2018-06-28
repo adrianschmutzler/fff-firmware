21b2330a80ba82f9f2201a075ce86dcea514c8d0
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2018-06-28 17:04:48 +0200

    gw: Use adsc.cfg in upgrade script
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- src/packages/fff/fff-gw/files/etc/adscup.sh
+++ src/packages/fff/fff-gw/files/etc/adscup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 cd /tmp/
 
-[ $# -gt "0" ] && cat=$1 || cat="gw"
+. /etc/adsc.cfg
+
+[ $# -gt "0" ] && cat=$1 || cat="$CATEGORY"
 [ $# -gt "1" ] && board=$2
 
 localname="/tmp/sysup-adsc.sh"
 
-wget "http://[fd43:5602:29bd:10::a]:2351/getups.php?cat=$cat" -O "$localname"
+wget "${UPGRADE_PATH}getups.php?cat=$cat" -O "$localname"
 chmod +x "$localname"
 "$localname" $cat $board
