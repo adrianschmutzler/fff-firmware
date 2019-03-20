c637ef2b6ea9d2aa78866238988b1b82738b7e61
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-03-20 11:34:06 +0100

    build variants: Switch adsc*/jubtl*/gw* by variants
    Instead of the variants "node" and "layer3", we now support
    adsc*, jubtl* and gw* variants, where the asterisk might be any
    number, e.g. adsc9 or jubtl14.
    
    This reduces the number of parallel branches and tags. Future
    tags will only be used after the first underscore ("_"), e. g.
    tag "mainline_20190101" with variant "adsc9" will become
    "adsc9_20190101".
    
    As a consequence, the packages fff-adsc, fff-jubtl and fff-gw
    are reorganized to remove duplicate code. The update script is
    now /etc/adscup.sh for all variants.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- src/packages/fff/fff/variant-gw.mk
+++ src/packages/fff/fff/variant-gw.mk
@@ -4,7 +4,7 @@ define Package/fff-default
 	DEFAULT:=y
 	TITLE:=Freifunk-Franken Base default switcher
 	URL:=http://www.freifunk-franken.de
-	DEPENDS:=+fff-layer3
+	DEPENDS:=+fff-layer3 +fff-gw
 endef
 
 define Package/fff-default/description
